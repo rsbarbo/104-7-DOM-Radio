73324b12fa2d91f7150d80c985880ebabff4c532
Raphael Barbo <rsbarbo@gmail.com>
2016-11-08 15:18:05 -0700

    update stylesheet and removed dropzone

--- assets/public/app.js
+++ assets/public/app.js
@@ -5,37 +5,45 @@ var files = [];
 function uploadHandler(path, status, xhr) {
   var file_parts = path.split(".");
   var extension = file_parts[file_parts.length - 1];
-  if(["mp3"].indexOf(extension.toLowerCase()) >= 0) {
+  if(["mp3", "m4a"].indexOf(extension.toLowerCase()) >= 0) {
     $("#file-listing").append("<li><a href='" + path + "' target='_blank'>" + path + "</a></li>");
   } else {
     $("#file-listing").append("<p>file format is not valid</p>");
   }
 };
 
+function fillForm(tag){
+  $('#ajax-attachment-upload-form input[name="album"]').val(tag.tags.album);
+  debugger;
+};
+
 // From remote host
 function readFile(mp3_file) {
+  var tag;
   jsmediatags.read(mp3_file, {
     onSuccess: function(tag) {
+      fillForm(tag);
       console.log(tag);
+      tag = tag;
     },
     onError: function(error) {
       console.log(error);
     }
   });
+  return tag;
 };
 
   $(function(){
 
     $("#attachment").change(function(event) {
       $.each(event.target.files, function(index, file) {
-        readFile(file);
+        var metaFile = readFile(file);
         var reader = new FileReader();
         reader.onload = function(event) {
           object = {};
           object.filename = file.name;
           object.data = event.target.result;
           files.push(object);
-          debugger;
         };
         reader.readAsDataURL(file);
       });
