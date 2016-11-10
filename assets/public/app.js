e102b97341647ffe879a635244b7ae58308e7e6f
Raphael Barbo <rsbarbo@gmail.com>
2016-11-10 01:59:53 -0700

    update ajax calls

--- assets/public/app.js
+++ assets/public/app.js
@@ -1,6 +1,18 @@
 var jsmediatags = window.jsmediatags;
 var files = [];
 
+// if request fails
+var onFail = function(error){
+  console.error(error);
+};
+
+// if request is sucessful
+var onGetSuccess = function(data){
+  if(data.id) {
+    $('#file-listing').append('<p class="post" style="text-align: center; color:red;"><b>' + "Song: " + data.title + ", " + "posted to database"+ '</b></p>')
+  }
+};
+
 function uploadHandler(path, status, xhr) {
   var file_parts = path.split(".");
   var extension = file_parts[file_parts.length - 1];
@@ -15,8 +27,9 @@ function fillForm(tag){
   $('#ajax-attachment-upload-form input[name="album"]').val(tag.tags.album);
   $('#ajax-attachment-upload-form input[name="title"]').val(tag.tags.title);
   $('#ajax-attachment-upload-form input[name="artist"]').val(tag.tags.artist);
-  debugger;
+  $('#ajax-attachment-upload-form input[name="genre"]').val(tag.tags.genre);
 };
+
 
 // From remote host
 function readFile(mp3_file) {
@@ -34,32 +47,54 @@ function readFile(mp3_file) {
   return tag;
 };
 
-  $(function(){
+$(function(){
 
-    $("#attachment").change(function(event) {
-      $.each(event.target.files, function(index, file) {
-        var metaFile = readFile(file);
-        var reader = new FileReader();
-        reader.onload = function(event) {
-          object = {};
-          object.filename = file.name;
-          object.data = event.target.result;
-          files.push(object);
-        };
-        reader.readAsDataURL(file);
-      });
+  $("#attachment").change(function(event) {
+    $.each(event.target.files, function(index, file) {
+      var metaFile = readFile(file);
+      var reader = new FileReader();
+      reader.onload = function(event) {
+        object = {};
+        console.log(object);
+        object.filename = file.name;
+        object.data = event.target.result;
+        files.push(object);
+      };
+      reader.readAsDataURL(file);
     });
+  });
 
-    $("#ajax-attachment-upload-form").submit(function(form) {
-      $.each(files, function(index, file) {
-        $.ajax({url: "http://71.229.150.148/uploadpublic",
-        type: 'POST',
-        data: {filename: file.filename, data: file.data},
-        crossDomain : true,
-        success: uploadHandler
-      });
-    });
+  $("#ajax-attachment-upload-form").submit(function(form) {
+
+    var album = document.getElementById("album").value;
+    var title = document.getElementById("title").value;
+    var artist = document.getElementById("artist").value;
+    var genre = document.getElementById("genre").value;
+    var email = document.getElementById("email").value;
+
+    var mp3_info = {album: album, title: title, artist: artist, genre: genre, email: email}
+
+    var newObject = {};
+
+    for (var attrname in mp3_info) { newObject[attrname] = mp3_info[attrname]; }
+    for (var attrname in files[0]) { newObject[attrname] = files[0][attrname]; }
+
+    $.ajax({url: "http://localhost:3000/api/v1/mp3s",
+    type: 'POST',
+    data: {filename: newObject.filename,
+      data: newObject.data,
+      album: newObject.album,
+      title: newObject.title,
+      artist: newObject.artist,
+      genre: newObject.genre,
+      email: newObject.email},
+    })
+    .done(onGetSuccess)
+    .fail(onFail);
     files = [];
     form.preventDefault();
+    setTimeout(function(){
+      document.getElementById("ajax-attachment-upload-form").reset();
+    },3700);
   });
 });
